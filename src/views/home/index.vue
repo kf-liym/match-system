29c62b7c9afa8107f8c54bae5d113568e86ca2e7
周星妤 <zhouxy@kamfu.com.cn>
2020-01-09 20:57:09 +0800

    步骤一更新

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <el-steps class="step" :active="active">
+    <el-steps
+      class="step"
+      :active="active"
+    >
       <el-step
         v-for="(item, index) in step"
         :key="index"
@@ -8,7 +11,12 @@
         @click.native="handleStep(index)"
       />
     </el-steps>
-    <el-alert class="alert" title="操作注意事项" type="warning" show-icon />
+    <el-alert
+      class="alert"
+      title="操作注意事项"
+      type="warning"
+      show-icon
+    />
     <ul class="description">
       <li>1、出质前盖坐着的著作权是否授权及授权情况说明：若未授权他人使用，填写“否”；若有授权情况，请在授权情况说明栏中填写著作权授权许可（包括专有）</li>
       <li>2、出质前盖坐着的著作权是否授权及授权情况说明：若未授权他人使用，填写“否”；若有授权情况，请在授权情况说明栏中填写著作权授权许可（包括专有）</li>
